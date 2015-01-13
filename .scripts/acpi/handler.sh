c433a72cf454bac0816b0cc23118cb324276fcce
Nuno Carvalho <ntfc30@gmail.com>
2015-01-13 21:37:11 +0000

    Fixed compton. Powerup/Powerdown

--- .scripts/acpi/handler.sh
+++ .scripts/acpi/handler.sh
@@ -9,16 +9,18 @@
 # $3 = 
 # $4 = 00000000 or 00000001
 
+USER=ntfc
+
 case "$1" in
   ac_adapter)
     case "$4" in
       00000000)
         # Battery mode
-        /home/nc/.scripts/power/powerdown &
+        /home/$USER/.scripts/power/powerdown &
       ;;
       00000001)
         # AC mode
-        /home/nc/.scripts/power/powerup &
+        /home/$USER/.scripts/power/powerup &
       ;;
     esac
   ;;
@@ -26,39 +28,30 @@ case "$1" in
     case "$2" in
       PBTN|PWRF)
         logger "Power button pressed"
-          su nc -c 'export DISPLAY=:0.0 ; /usr/bin/pygtk-shutdown-systemd'
+          su $USER -c 'export DISPLAY=:0.0 ; /usr/bin/pygtk-shutdown-systemd'
 
       ;;
     esac
   ;;
-  hotkey)
-    case "$3" in
-      00000034) # screen off
-        su nc -c 'export DISPLAY=:0.0 ; xscreensaver-command -lock' &
-      ;;
-      00000033) # screen on
-      ;;
-      00000040|00000041|00000043|00000045) # multimedia keys
-        /home/nc/.scripts/acpi/mpd-control "$3"
-      ;;
-      00000032) # mute
-        su nc -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u nc`"/pulse/" ; \
+  video/brightnessup)
+    light -A 10 &
+  ;;
+  video/brightnessdown)
+    light -U 10 &
+  ;;
+  button/volumeup)
+        su $USER -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u $USER`"/pulse/" ; \
           export DISPLAY=:0.0 ; \
-          /home/nc/.scripts/pacontrol mute 1' &
-      ;;
-      00000031) # vol down
-        su nc -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u nc`"/pulse/" ; \
+          /home/$USER/.scripts/pacontrol up 1' &
+  ;;
+  button/volumedown)
+        su $USER -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u $USER`"/pulse/" ; \
           export DISPLAY=:0.0 ; \
-          /home/nc/.scripts/pacontrol down 1' &
-      ;;
-      00000030) # vol up
-        su nc -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u nc`"/pulse/" ; \
+          /home/$USER/.scripts/pacontrol down 1' &
+  ;;
+  button/mute)
+        su $USER -c 'export PULSE_RUNTIME_PATH="/run/user/"`id -u $USER`"/pulse/" ; \
           export DISPLAY=:0.0 ; \
-          /home/nc/.scripts/pacontrol up 1' &
-      ;;
-      00000055) # calculator
-        su nc -c 'export DISPLAY=:0.0 ; /usr/bin/speedcrunch' &
-    esac
+          /home/$USER/.scripts/pacontrol mute 1' &
   ;;
 esac
-
