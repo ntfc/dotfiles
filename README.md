0a9ecaa45d3c27fd00a0baa8631f51adbe73837a
Mathieu Ruellan <mathieu.ruellan@gmail.com>
2015-03-23 19:56:33 +0100

    redirect logger to host syslog

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ Build:
 
     $ git pull https://github.com/mathieuruellan/acestream_docker.git
     $ cd acestream_docker
-    $ docker build -t mathieuruellan/acestream-docker .
+    $ docker build -t mathieuruellan/acestream .
 
 Launch:
 
-    $ docker  run -d --name=acestream  --net=host -ti mathieuruellan/acestream-docker
+    $ fig up -d
 
--- launch.sh
+++ launch.sh
@@ -2,5 +2,5 @@ DIR=$(dirname $(readlink -f "$0"))
 DIR="$DIR/acestream"
 while :
 do
-  ${DIR}/acestreamengine --lib-path ${DIR}/ --bind-all --client-console --live-cache-type memory --live-cache-size 209715200 ----max-peers 200 --max-peers-limit 101
+  ${DIR}/acestreamengine --lib-path ${DIR}/ --bind-all --client-console --live-cache-type memory --live-cache-size 209715200 ----max-peers 200 --max-peers-limit 100 |logger -t acestream
 done
