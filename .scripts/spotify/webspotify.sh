146f699fc241dbaff2d062f6b0c493570a1a0788
Nuno Carvalho <ntfc30@gmail.com>
2015-08-18 11:24:43 +0100

    switch back to the desktop where we were before

--- .scripts/spotify/webspotify.sh
+++ .scripts/spotify/webspotify.sh
@@ -27,6 +27,8 @@ esac
 
 webplayer_winid="$(wmctrl -lx | grep Spotify | grep crx_ | awk '{print $1}')"
 if [[ ! -z "$webplayer_winid" ]]; then
+  current_desktop=$(xdotool get_desktop)
   xdotool windowactivate --sync $webplayer_winid key "$key"
   xdotool windowminimize $webplayer_winid
+  xdotool set_desktop $current_desktop
 fi
