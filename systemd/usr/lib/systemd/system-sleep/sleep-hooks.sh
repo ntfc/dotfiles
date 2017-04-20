8dc3c98199893034bd575329364077662b7aa9ed
Nuno Carvalho <me@ntfc.me>
2017-04-20 15:12:08 +0100

    Moved systemd custom scripts

--- systemd/usr/lib/systemd/system-sleep/sleep-hooks.sh
+++ systemd/usr/lib/systemd/system-sleep/sleep-hooks.sh
@@ -4,6 +4,8 @@
 # $1 = pre or post
 # $2 = suspend, hibernate or hybrid-sleep
 
+set -x
+
 USER=$(who -s | grep "(:0)" | awk '{print $1}')
 
 case $1/$2 in
@@ -12,6 +14,9 @@ case $1/$2 in
     if [[ ! -z "`pidof spotify`" ]]; then
       su $USER -c 'export DISPLAY=:0.0; spotifycli -s' &
     fi
+    # pause VLC. Taken from http://stackoverflow.com/a/43156436
+    # TODO: need to sort out the dbus mess..
+    dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
     ;;
   post/*)
     # not working???? maybe AC_STATE is not updated? wtf?
