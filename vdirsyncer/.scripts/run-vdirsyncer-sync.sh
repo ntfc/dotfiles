0aa8bac3eb974ce72c0c4e10626727113386e6f7
Nuno Carvalho <me@ntfc.me>
2017-04-20 15:33:35 +0100

    Update script to fetch radicale calendar through cron jobs

--- vdirsyncer/.scripts/run-vdirsyncer-sync.sh
+++ vdirsyncer/.scripts/run-vdirsyncer-sync.sh
@@ -5,7 +5,7 @@ set -e
 if hash nc 2>/dev/null; then
   # check for internet connectivity
   # from http://unix.stackexchange.com/a/190610
-  if nc -dzw1 google.com 443 && echo |openssl s_client -connect $test:443 2>&1 |awk 'handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 } $1 $2 == "SSLhandshake" { handshake = 1 }'; then
+  if nc -zw1 google.com 443 && echo |openssl s_client -connect $test:443 2>&1 |awk 'handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 } $1 $2 == "SSLhandshake" { handshake = 1 }'; then
     vdirsyncer sync 
     vdirsyncer metasync
   else
